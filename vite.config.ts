88c1267d65f07937b0c76cc1bdc92066dd148908
mohammad yazdkhasti <152327337+Mehmetttzd@users.noreply.github.com>
2024-12-10 12:30:22 -0500

    Push portfolio files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/portfolio-site/' : '/', // Use base path only in production
+  base: '/portfolio-site/', // GitHub Pages expects this to match your repo name
   plugins: [react()],
 })
